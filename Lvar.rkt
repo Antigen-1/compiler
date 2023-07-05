76167063f7cabc36c0cf145ec5bad56814061c28
Antigen-1 <zhanghao@antigen.top>
2023-07-05 23:21:26 +0800

    update Lvar and Lvar_mon

--- Lvar.rkt
+++ Lvar.rkt
@@ -1,5 +1,5 @@
 #lang racket/base
-(require racket/contract "pkg.rkt" racket/match racket/list)
+(require racket/contract "pkg.rkt" racket/match)
 (provide install-Lvar install-Lvar_mon)
 
 (define (install-language name contract interpreter . passes)
@@ -7,25 +7,30 @@
 
 ;;Lvar
 ;;------------------------------------------------------------------------------------
+;;contracts for Lvar and Lvar_mon
+(define primitive? (or/c symbol? fixnum?))
+(define atomic?
+  (or/c primitive?
+        (list/c 'read) (list/c '- primitive?) (list/c '- primitive? primitive?) (list/c '+ primitive? primitive?)))
+(define mon? (opt/c (recursive-contract (or/c atomic? (list/c 'let (list/c (list/c symbol? mon?)) mon?)) #:flat)))
+
 (define (install-Lvar)
   (define form?
     (opt/c
      (recursive-contract
-      (or/c fixnum? (list/c 'read) symbol?
-            (list/c '- form?) (list/c '- form? form?) (list/c '+ form? form?)
+      (or/c primitive?
+            (list/c '- form?) (list/c '- form? form?) (list/c '+ form? form?) (list/c 'read)
             (list/c 'let (list/c (list/c symbol? form?)) form?)))))
   
   (define (Lvar-interpret form)
     (eval form (make-base-namespace)))
 
   (define (reference v t)
-    (cond ((and (symbol? v) (hash-has-key? t v))
-           (hash-ref t v))
+    (cond ((and (symbol? v) (hash-ref t v #f)))
           ((symbol? v) (raise (make-exn:fail:contract:variable v "not yet defined" (current-continuation-marks))))
           (else v)))
   (define (n:gensym s)
     (string->symbol (symbol->string (gensym s))))
-  (define (primitive? v) (or (symbol? v) (fixnum? v) (equal? v '(read))))
   
   #; (-> Lvar |restricted Lvar|)
   (define (uniquify form (table (hasheq)))
@@ -41,33 +46,30 @@
       (v (reference v table))))
   #; (-> Lvar Lvar_mon)
   (define (remove-complex-operands form)
-    (match form
-      ((list 'let (list (list v e)) f)
-       (list 'let (list (list v (if (primitive? e) e (remove-complex-operands e))))
-             (if (primitive? f) f (remove-complex-operands f))))
-      ((list '- form)
-       (if (primitive? form)
-           (list '- form)
+    (if (mon? form)
+        form
+        (match form
+          ((list 'let (list (list v e)) f)
+           (list 'let (list (list v (remove-complex-operands e)))
+                 (remove-complex-operands f)))
+          ((list '- form)
            (let ((nv (n:gensym 'tmp)))
              (list 'let (list (list nv (remove-complex-operands form)))
-                   (list '- nv)))))
-      ((list op form1 form2)
-       (cond ((and (primitive? form1) (primitive? form2))
-              (list op form1 form2))
-             ((primitive? form1)
-              (let ((nv (n:gensym 'tmp)))
-                (list 'let (list (list nv (remove-complex-operands form2)))
-                      (list op form1 nv))))
-             ((primitive? form2)
-              (let ((nv (n:gensym 'tmp)))
-                (list 'let (list (list nv (remove-complex-operands form1)))
-                      (list op nv form2))))
-             (else (let ((nv1 (n:gensym 'tmp))
-                         (nv2 (n:gensym 'tmp)))
-                     (list 'let (list (list nv1 (remove-complex-operands form1)))
-                           (list 'let (list (list nv2 (remove-complex-operands form2)))
-                                 (list op nv1 nv2)))))))
-      (v v)))
+                   (list '- nv))))
+          ((list op form1 form2)
+           (cond ((primitive? form1)
+                  (let ((nv (n:gensym 'tmp)))
+                    (list 'let (list (list nv (remove-complex-operands form2)))
+                          (list op form1 nv))))
+                 ((primitive? form2)
+                  (let ((nv (n:gensym 'tmp)))
+                    (list 'let (list (list nv (remove-complex-operands form1)))
+                          (list op nv form2))))
+                 (else (let ((nv1 (n:gensym 'tmp))
+                             (nv2 (n:gensym 'tmp)))
+                         (list 'let (list (list nv1 (remove-complex-operands form1)))
+                               (list 'let (list (list nv2 (remove-complex-operands form2)))
+                                     (list op nv1 nv2))))))))))
 
   (install-language 'Lvar form? Lvar-interpret (cons 'uniquify uniquify) (cons 'remove-complex-operands remove-complex-operands)))
 ;;------------------------------------------------------------------------------------
@@ -75,27 +77,23 @@
 ;;Lvar_mon
 ;;------------------------------------------------------------------------------------
 (define (install-Lvar_mon)
-  (define primitive? (or/c symbol? fixnum?))
-  (define atomic? (or/c primitive? (list/c '- primitive?) (list/c '- primitive? primitive?) (list/c '+ primitive? primitive?) (list/c 'read)))
-  (define form? (opt/c (recursive-contract (or/c atomic? (list/c 'let (list/c (list/c symbol? form?)) form?)))))
 
   (define (Lvar_mon-interpret form)
     (eval form (make-base-namespace)))
   
   #; (-> Lvar_mon Cvar)
   (define (explicate-control form)
-    (let-values (((former latter)
-                  (split-at-right
-                   (let loop ((form form))
-                     (match form
-                       ((list 'let (list (list v e)) f)
-                        (if (atomic? e)
-                            (cons (list 'define v e) (loop f))
-                            (let-values (((former latter) (split-at-right (loop e) 1)))
-                              (append former (cons (list 'define v (car latter)) (loop f))))))
-                       (v (list v))))
-                   1)))
-      (list 'program (list 'start (append former (list (cons 'return latter)))))))
+    (define (make-continuation ret expr)
+      (if ret (list 'define ret expr) (list 'return expr)))
+    
+    (let ((seq
+           (let loop ((ret #f) (form form))
+             (if (atomic? form)
+                 (list (make-continuation ret form))
+                 (match form
+                   ((list 'let (list (list v e)) f)
+                    (append (loop v e) (loop ret f))))))))
+      (list 'program (list 'start seq))))
 
-  (install-language 'Lvar_mon form? Lvar_mon-interpret (cons 'explicate-control explicate-control)))
+  (install-language 'Lvar_mon mon? Lvar_mon-interpret (cons 'explicate-control explicate-control)))
 ;;------------------------------------------------------------------------------------
